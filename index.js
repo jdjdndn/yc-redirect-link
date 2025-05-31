a8146e1eac198d633ffd1f76954f29dab675e008
yu cheng <2667389861@qq.com>
2025-05-31 18:53:34 +0800

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         yc-替换真实链接
+// @name         替换真实链接(yc-redirect-links)
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  尝试替换页面上的重定向链接为真实链接
